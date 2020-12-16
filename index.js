c0beb803675acac8e661a12251a1fb751fe77d63
Danilo Aleixo <danilo_aleixo@hotmail.com>
2020-12-16 19:46:50 -0300

    v-for

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var app = new Vue({
   el: '#app',
   data: {
-    seen: true
+    todos: [
+      { text: 'Learn JavaScript' },
+      { text: 'Learn Vue' },
+      { text: 'Build something awesome' }
+    ]
   }
-})
+});
+
